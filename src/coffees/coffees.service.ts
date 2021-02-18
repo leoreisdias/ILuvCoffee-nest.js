ceb85517a2fbf4fc541118d86e37b1d19431f509
leoreisdias <leonardord99@gmail.com>
2021-02-18 16:10:35 -0300

    ValidationPipes options -> whiteList: possibilita ignorar elementos inexperados -> forbidNonWhiteListed: não aceita requisição em caso de elemento inexperado

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -29,6 +29,8 @@ export class CoffeesService {
 
     create(createCoffeeDto: any){
         this.coffees.push(createCoffeeDto);
+
+        return createCoffeeDto;
     }
 
     update(id: string, updateCoffeeDto: any){
