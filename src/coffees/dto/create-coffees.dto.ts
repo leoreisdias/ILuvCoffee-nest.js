d48fe094bc8a8ff111d809f2595e1f9ea309a1be
leoreisdias <leonardord99@gmail.com>
2021-02-18 16:00:57 -0300

    Class Validator, Transformer e Mapped-Types (evitar redundancias)

--- src/coffees/dto/create-coffees.dto.ts
+++ src/coffees/dto/create-coffees.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCoffeesDto {}
+import { IsString } from "class-validator";
+
+export class CreateCoffeesDto {
+    @IsString()
+    readonly name: string;
+
+    @IsString()
+    readonly brand: string;
+
+    @IsString({each: true})
+    readonly flavors: string[];
+}
--- src/coffees/dto/update-coffees.dto.ts
+++ src/coffees/dto/update-coffees.dto.ts
@@ -1,5 +1,4 @@
-export class UpdateCoffeesDto {
-   readonly name?: string;
-   readonly brand?: string;
-   readonly flavors?: string[];
-}
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateCoffeesDto } from "./create-coffees.dto";
+
+export class UpdateCoffeesDto extends PartialType(CreateCoffeesDto){}
